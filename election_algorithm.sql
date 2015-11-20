4b212a90080042a7b8e8b5a6bc293d244f906dae
Lukas Vogel <lukas@vogelnest.org>
2015-11-20 02:44:19 +0100

    election algorithm modularized

--- election_algorithm.sql
+++ election_algorithm.sql
@@ -17,24 +17,34 @@ ALTER FUNCTION nats(numeric)
   OWNER TO postgres;
 
 
+
+
+/* The winners of a direct mandate */
+
+CREATE OR REPLACE VIEW directmandate_winners AS
+ SELECT w.bundesland,
+    w.id AS wahlkreis,
+    d.candidate,
+    d.party
+  FROM erststimme_results er,
+    directmandate d,
+    wahlkreis w
+  WHERE er.candidate = d.candidate AND d.election = er.election AND er.wahlkreis = w.id AND er.election = 2 AND NOT (EXISTS ( SELECT er2.candidate,
+            er2.wahlkreis,
+            er2.election,
+            er2.count
+           FROM erststimme_results er2
+          WHERE er2.wahlkreis = er.wahlkreis AND er2.election = er.election AND er2.count > er.count));
+
+ALTER TABLE directmandate_winners
+  OWNER TO postgres;
+
+
+
+
 /* The following two VIEWs are needed for the party divisor calculation function */
 
 CREATE OR REPLACE VIEW mandates_party_bland AS
- WITH directmandate_winners AS (
-         SELECT w.bundesland,
-            w.id AS wahlkreis,
-            d.candidate,
-            d.party
-           FROM erststimme_results er,
-            directmandate d,
-            wahlkreis w
-          WHERE er.candidate = d.candidate AND d.election = 2 AND er.wahlkreis = w.id AND er.election = 2 AND NOT (EXISTS ( SELECT er2.candidate,
-                    er2.wahlkreis,
-                    er2.election,
-                    er2.count
-                   FROM erststimme_results er2
-                  WHERE er2.wahlkreis = er.wahlkreis AND er2.election = 2 AND er2.count > er.count))
-        )
  SELECT dw.bundesland,
     dw.party,
     count(*) AS mandates
@@ -55,20 +65,6 @@ CREATE OR REPLACE VIEW votes_bundesland AS
          SELECT sum(zr2.count) AS total
            FROM zweitstimme_results zr2
           WHERE zr2.election = 2
-        ), directmandate_winners AS (
-         SELECT w.bundesland,
-            w.id AS wahlkreis,
-            d.candidate,
-            d.party
-           FROM erststimme_results er,
-            directmandate d,
-            wahlkreis w
-          WHERE er.candidate = d.candidate AND d.election = 2 AND er.wahlkreis = w.id AND er.election = 2 AND NOT (EXISTS ( SELECT er2.candidate,
-                    er2.wahlkreis,
-                    er2.election,
-                    er2.count
-                   FROM erststimme_results er2
-                  WHERE er2.wahlkreis = er.wahlkreis AND er2.election = 2 AND er2.count > er.count))
         ), im_bundestag AS (
          SELECT v.party
            FROM votesbyparty v,
@@ -91,7 +87,6 @@ CREATE OR REPLACE VIEW votes_bundesland AS
 
 ALTER TABLE votes_bundesland
   OWNER TO postgres;
-
 
 
 
@@ -150,9 +145,7 @@ $BODY$
 
 
 
-/* The view specifying the elected bundestag-candidates for 2013 */
-CREATE OR REPLACE VIEW members_of_bundestag_2013 AS (
-
+CREATE OR REPLACE VIEW seats_by_party_2013 AS
 with votesbyparty as
 	(select zr.party, sum(count) as votes
 	from zweitstimme_results zr
@@ -165,29 +158,13 @@ with votesbyparty as
          where zr2.election = 2
         ),
 
-
-     directmandate_winners as ( /* The winners of a direct mandate in each wahlkreis and his party (if he has one) */
-	select w.bundesland, w.id as wahlkreis, d.candidate, d.party
-	from erststimme_results er, directmandate d, wahlkreis w
-	where er.candidate = d.candidate
-	and d.election = 2
-	and er.wahlkreis = w.id
-	and er.election = 2
-	and not exists (
-		select *
-		from erststimme_results er2
-		where er2.wahlkreis = er.wahlkreis
-		and er2.election = 2
-		and er2.count > er.count)
-     ),
-
     parties_in_bundestag as /* Parties that may get seats in the bundestag */
 	(select v.party
 	from votesbyparty v, totalvotes t
 	where v.votes >= (t.total * 1.00 / 100 * 5) /* all parties with more than 5% of total zweitstimmen */
 	UNION
 	select party
-	from directmandate_winners
+	from directmandate_winners dw
 	group by party
 	having count(*) >= 3 /* all parties with 3 or more direct mandates */
 	UNION
@@ -196,14 +173,6 @@ with votesbyparty as
   where v.party = p.id
   and p.isMinorityParty /* all minority parties are exempt from the 5% clause */
 	),
-
-    votes_bundesland as ( /* votes per bundesland and party */
-	select wk.bundesland,zr.party, sum(count) as votes
-	from zweitstimme_results zr, wahlkreis wk, parties_in_bundestag ib
-	where zr.wahlkreis = wk.id
-	and zr.party = ib.party
-	and zr.election = 2
-	group by wk.bundesland,zr.party),
 
      lague_ranking as ( /* parties ranked after the sainte-lague procedure by bundesland*/
 	select bundesland, party, rank() over (partition by bundesland order by votes / c.lague_coeff * 1.00 desc) as rank
@@ -231,13 +200,6 @@ with votesbyparty as
 	(16,17) /* Thüringen */
 	) as zuteilung(Bundesland,Sitze) /* Sum: 598 Sitze, minimum amount of seats in the Bundestag */
      ),
-
-     mandates_party_bland as ( /* amount of direct mandates per party and bundesland */
-	select bundesland, party, count(*) as mandates
-	from directmandate_winners dw
-	group by bundesland, party
-     ),
-
      pseudodistribution_zw as ( /* distribution of seats taking no direct mandates into consideration*/
 	select r.bundesland, party, count(*) as seats
 	from lague_ranking r, initial_seats a
@@ -261,18 +223,29 @@ with votesbyparty as
 	select round(min(vp.votes / (m.minsitze - 0.5)),2) as bundesdivisor /* truncate because of possible floating point errors TODO: reason about this carefully! :) */
 	from least_num_seats m, votesbyparty vp
 	where m.party = vp.party
-    ),
+    )
+ SELECT vp.party,
+        CASE
+            WHEN (vp.votes * 1.00 / 2::numeric) > totalvotes.votes THEN round(totalvotes.votes / (d.bundesdivisor * 2::numeric), 0) + 1::numeric
+            ELSE round(vp.votes / d.bundesdivisor, 0)
+        END AS seats
+   FROM votesbyparty vp,
+    parties_in_bundestag ib,
+    bundesdivisor d,
+    ( SELECT sum(votes_bundesland.votes) AS votes
+           FROM votes_bundesland) totalvotes
+  WHERE vp.party = ib.party;
 
-    total_num_seats as ( /* the corrected number of seats per party*/
-	select vp.party,
-  (case when (vp.votes*1.00/2 > totalvotes.votes) then round(totalvotes.votes/(d.bundesdivisor*2),0)+1 /* Mehrheitsklausel */
-                                         else  round(vp.votes/d.bundesdivisor,0) end ) seats
-	from votesbyparty vp, parties_in_bundestag ib, bundesdivisor d, (select sum(votes) as votes from votes_bundesland) totalvotes
-	where vp.party = ib.party
-    ),
+ALTER TABLE seats_by_party_2013
+  OWNER TO postgres;
 
-    partydivisor as ( /* the amount of votes needed to get one seat for each party */
-	select * from total_num_seats total, find_partydivisor(total.party , total.seats) as divisor
+
+
+
+/* The view specifying the elected bundestag-candidates for 2013 */
+CREATE OR REPLACE VIEW members_of_bundestag_2013 AS (
+  with partydivisor as ( /* the amount of votes needed to get one seat for each party */
+	select * from seats_by_party_2013 total, find_partydivisor(total.party , total.seats) as divisor
     ),
 
     seats_bland as ( /* the final amount of seats each party gets in each bundesland */
@@ -291,7 +264,7 @@ with votesbyparty as
 
     members_of_bundestag as (
 	select bundesland, candidate, party
-	from directmandate_winners dw
+	from directmandate_winners
 	UNION
 	select sb.bundesland, rc.candidate, rc.party
 	from remaining_cand_on_ll rc, seats_bland sb left join mandates_party_bland mpb on mpb.bundesland = sb.bundesland and mpb.party = sb.party
